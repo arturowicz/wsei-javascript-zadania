373dc7e29fe7e81a1119c1eeba2e4787e0ec1378
arturowicz <artur.teo@wp.pl>
2019-10-06 10:15:50 +0200

    Add exercise 1 fix

--- 1_Zadania/obowiązkowe/1_Funkcje_czasu/js/app.js
+++ 1_Zadania/obowiązkowe/1_Funkcje_czasu/js/app.js
@@ -1,6 +1,6 @@
 // Zadanie 1
 function countHello(number) {
-    var counter = 1;
+    var counter = 0;
     var countdown = setInterval(() => {
         console.log("Hello " + counter);
         counter++
