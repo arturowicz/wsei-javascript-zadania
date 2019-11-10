11b2350066f84a64fda9bffae07196e4852620f0
arturowicz <artur.teo@wp.pl>
2019-11-10 11:26:30 +0100

    Add exercise 4.1 4.2

---
+++ 1_Zadania/obowiązkowe/4_Obiekty/js/app.js
@@ -0,0 +1,48 @@
+// Zadanie 0a
+
+var city = {
+    capital: "Warszawa",
+    population: 37000000,
+    president: "Andrzej Duda",
+    primeMinisters: []
+}
+
+console.log(city);
+
+// Zadanie 0b
+
+var timeMachine = {
+    shape: "Box",
+    model: "v1",
+    run: (date, place) => {}
+}
+
+console.log(timeMachine.shape);
+console.log(timeMachine.model);
+timeMachine.run(1900, "London");
+
+// Zadanie 1
+
+var book = {
+    title: "Harry Potter",
+    author: "J.K Rowling",
+    numberOfPages: 500
+}
+
+console.log(book.title);
+console.log(book.author)
+console.log(book.numberOfPages);
+
+// Zadanie 2
+
+var person = {
+    name: "Artur",
+    age: "21",
+    sayHello : () => { console.log("hello") }
+}
+
+console.log(person.name);
+console.log(person.age);
+person.sayHello();
+
+// Zadanie 3
